57adc11e4c525265279d823dc9a8adbedb29fc9e
Bruno Magrini <brmagrini@gmail.com>
2017-03-10 18:00:56 -0300

    cria cpt e taxonomia. inicia html php css

---
+++ archive-fonte.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each specific one. For example, Twenty Thirteen
+ * already has tag.php for Tag archives, category.php for Category archives,
+ * and author.php for Author archives.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header('internas'); ?>
+	<main id="content" class="row" tabindex="-1" role="main">
+			<?php if ( have_posts() ) : ?>
+				<h2 class="titulo">Banco de fontes</b></h2>
+			
+				<div class="barra-busca">
+					<h6>Busque fontes: </h6>
+				<form method="get" class="form-busca" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+					<label for="navbar-search" class="sr-only">
+						<?php _e( 'Search:', 'odin' ); ?>
+					</label>
+					<div class="form-group">
+						<input type="search" value="<?php echo get_search_query(); ?>" class="form-control" name="s" id="navbar-search" />
+					</div>
+					<button type="submit" class="btn botao-busca"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/lupa.png" alt=""></button>
+				</form>
+					<h6>Temas: </h6>
+					 <?php $terms = get_terms( 'tema_fonte' );
+					if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+					    echo '<select>';
+					    foreach ( $terms as $term ) {
+					        echo '<option>' . $term->name . '</option>';
+					    }
+					    echo '</select>';
+					}?> 
+
+
+					<?php
+						$terms = get_terms([
+    						'taxonomy' => "tema_fonte",
+    						
+						]);
+						$count=count($terms);
+						foreach ($terms as $term) {
+							?>
+							<a href="<?php  echo get_term_link($term->term_id);?>"><?php echo $term->name; ?></a>
+							<?php 
+						}
+						print_r($terms);
+					?>
+					<div class="clearfix"></div>
+				</div>
+				<div>
+				<?php
+					// echo '<pre>';
+					// print_r(;
+					// echo '</pre>';
+						// Start the Loop.
+						while ( have_posts() ) : the_post();
+
+							/*
+							 * Include the post format-specific template for the content. If you want to
+							 * use this in a child theme, then include a file called called content-___.php
+							 * (where ___ is the post format) and that will be used instead.
+							 */
+							get_template_part( 'content', 'fonte' );
+
+
+						endwhile;
+
+						// Post navigation.
+						odin_paging_nav();
+
+					else :
+						// If no content, include the "No posts found" template.
+						get_template_part( 'content', 'none-banco' );
+
+				endif;
+			?>
+			</div>
+
+	</main>
+
+<?php
+get_footer();
---
+++ content-fonte.php
@@ -0,0 +1,16 @@
+<div class="cada-publicacao-archive col-sm-6">
+
+	
+	 <div class="col-md-9 titulo-publicacao-archive">
+	 	<a href="<?php echo get_permalink( );?>">
+	 	<?php the_title( '<h4>', '</h4>'  ); 
+ 		?>
+ 		</a>
+ 		<?php
+
+		the_content( );
+	 	?>
+	</div>	
+	
+
+</div><!--cada-slide-2-->								
